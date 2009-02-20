77d42585c9a322c9085f7073d200eda8d16e0211
Leon.van.der.Ree <Leon.van.der.Ree@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-02-20 11:09:51 -0600

    start of implementing nice syntax for parsing object- and propertypaths to make the DataSourcePropel capable of hydrating objects.
    The constructor can't parse strings anymore... I will continue working at it from home.
    
    git-svn-id: http://svn.symfony-project.com/plugins/sfGridPlugin/trunk@15662 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/datasource/sfDataSourcePropel.class.php
+++ lib/datasource/sfDataSourcePropel.class.php
@@ -74,26 +74,181 @@ class sfDataSourcePropel extends sfDataSource
    * @var PropelPDO 
    */
   protected $connection = null;
+
+  /**
+   * Resolves the (last) ClassName from the objectPath
+   *
+   * @param string $objectPath an objectPath, with syntax baseClassName.ChildClassName.ChildClassName
+   *                           the childClassNames should have a getter in the baseClass
+   * @return string            the ClassName
+   */
+  static public function resolveClassNameFromObjectPath($objectPath)
+  {
+    // get the latest classReference (the part after the last '.')
+    $classReferences = explode('.', $objectPath);
+    $classReference = $classReferences[count($classReferences)-1];
     
+    // if there are multiple references to the same table, remove the RelatedBy.... part
+    $classParts = explode('RelatedBy', $classReference);
+    $className = $classParts[0];
+    
+    return $className;
+  }
+  
+  /**
+   * Resolves the Add-method for the last relation in an objectPath
+   *
+   * @param string $objectPath an objectPath, with syntax baseClassName.ChildClassName.ChildClassName
+   *                           the childClassNames should have a getter in the baseClass
+   * @return string            the add-Method of the child to register itself to its parent  
+   */
+  static public function resolveAddMethodFromObjectPath($objectPath)
+  {
+    // get the latest classReference (the part after the last '.')
+    $classReferences = explode('.', $objectPath);
+    $className = $classReferences[0];
+    $classReference = $classReferences[count($classReferences)-1];
+    
+    $related = '';
+    // if there are multiple references to the same table, remove the RelatedBy.... part
+    $classParts = explode('RelatedBy', $classReference);
+    if (count($classParts)>1)
+    {
+      $related = 'RelatedBy'.$classParts[1]; 
+    }
+    
+    return 'add'.$className.$related;
+  }
+  
+  /**
+   * Tests if the object path is valid, if not throws an exception 
+   *
+   * @param string $objectPath an objectPath, with syntax baseClassName.ChildClassName.ChildClassName
+   *                           the childClassNames should have a getter in the baseClass
+   * 
+   */
+  static public function checkObjectPath($objectPath)
+  {
+    $classReferences = explode('.', $objectPath, 2);
+    
+    // if not path was provided
+    if (count ($classReferences) == 0)
+    {
+      throw new UnexpectedValueException('empty path was provided');
+    }
+    
+    $className = $classReferences[0];
+    // then it must be an existing class
+    if (!class_exists($className))
+    {
+      throw new LogicException(sprintf('Class "%s" does not exist', $className));
+    }    
+
+    // class should be extension of Propel BaseObject
+    $reflection = new ReflectionClass($className);
+    // that class must be a child of Propels BaseObject
+    if (!$reflection->isSubclassOf('BaseObject'))
+    {
+      throw new LogicException(sprintf('Class "%s" is no Propel based class', $className));
+    }    
+    
+    if (isset($classReferences[1]))
+    {
+      $relatedClassReferences = explode('.', $classReferences[1], 2);
+      $relatedClassGetter = $relatedClassReferences[0]; 
+      // test with reflection if getter exists for ClassName
+      $getterMethod = 'get'.$relatedClassGetter;
+      if (!$reflection->hasMethod($getterMethod))
+      {
+        throw new LogicException(sprintf('Class "%s" has no method called "%s". Tip: you can add your own get-method that returns the related object.', $className, $getterMethod));
+      }
+      
+      // get directly related ClassName 
+      $partialObjectPath = $className . '.' .$relatedClassGetter; 
+      $relatedClassName = self::resolveClassNameFromObjectPath($partialObjectPath);
+      $addMethod = self::resolveAddMethodFromObjectPath($partialObjectPath);
+      
+      // test here for add-er, other validation checks are done with recursion 
+      $relatedReflection = new ReflectionClass($relatedClassName);
+      if (!$relatedReflection->hasMethod($addMethod))
+      {
+        throw new LogicException(sprintf('Class "%s" has no method called "%s. Tip: you can add your own add-method, required to perform the hydration.', $relatedClassName, $addMethod));
+      }
+      //recursively check
+      $relatedClassPath = $relatedClassName;
+      if (isset($relatedClassReferences[1]))
+      {
+        $relatedClassPath .= '.'.$relatedClassReferences[1]; 
+      }
+      self::checkObjectPath($relatedClassPath);
+    }
+    
+    // done, sucessfully parsed the objectPath
+  }
+  
+  /**
+   * returns an array of Classes refered to by an objectPath
+   *
+   * @param string $objectPath an objectPath, with syntax baseClassName.ChildClassName.ChildClassName
+   *                           the childClassNames should have a getter in the baseClass
+   * @param array $classes     an instance of the classes to be returned, that will be complemented
+   * @return unknown
+   */
+  static public function resolveAllClasses($objectPath, $classes = array())
+  {
+    $classReferences = explode('.', $objectPath, 2);
+    $relationName = $classReferences[0];
+
+    // add Class to array, if not already known
+    if (!isset($classes[$relationName]))
+    {
+      $classes[$relationName] = array('className' =>  self::resolveClassNameFromObjectPath($classReferences[0]),
+                                      'relatedTo' => array()
+                                );
+    }
+    
+    if (isset($classReferences[1]))
+    {
+      $relatedClassReferences = explode('.', $classReferences[1], 2);      
+      
+      $relatedTo = $relatedClassReferences[0];
+      if (!in_array($relatedTo, $classes[$relationName]['relatedTo']))
+      { 
+        $classes[$relationName]['relatedTo'][] = $relatedTo;
+      } 
+      
+      $classes = self::resolveAllClasses($classReferences[1], $classes);
+    }
+    
+    return $classes;
+  }
+  
   /**
    * Constructor.
    * 
-   * The data source can be given as a table-name, or a Criteria object.
-   * If you pass a Criteria object, it will be cloned because it needs to be 
-   * modified internally.
+   * The data source can be given as an (array of) objectPaths, or a custom 
+   * Criteria object. Custom criteria objects will not get hydrated, objects 
+   * names are!
+   * the Criteria object will be cloned, since it will be modified internally.
    * 
    * <code>
-   * // fetches all user objects
-   * $source = new sfDataSourcePropel('User');
+   * // fetches all user objects, and their related userProfiles from objectPath
+   * $source = new sfDataSourcePropel('User', 'User.UserProfile');
+   * // exactly the same:
+   * $source = new sfDataSourcePropel(array('User.UserProfile'));
+   * // this source->current() will return a hydrated object of the base object (User)
    * 
    * // fetches user objects from Criteria
    * $criteria = new Criteria();
    * UserPeer::addSelectColumns($criteria);
+   * // you can add more related / custom columns here
    * 
    * $countCriteria = new Criteria();
    * $countCriteria->setPrimaryTableName(UserPeer::TABLE_NAME);
    * 
    * $source = new sfDataSourcePropel($criteria, $countCriteria);
+   * // this source will contain non-hydrated resultsets,
+   * // hasColumn will only accept the tablename.COLUMNNAME syntax (from propel) 
    * </code>
    * 
    * @param  mixed $source             The data source 
@@ -111,35 +266,60 @@ class sfDataSourcePropel extends sfDataSource
    */
   public function __construct($source, $countCriteria = null)
   {
-    // the source can be passed as model class name...
+    // if provided object path(s), make it an array
     if (is_string($source))
     {
-      // then it must be an existing class
-      if (!class_exists($source))
-      {
-        throw new UnexpectedValueException(sprintf('Class "%s" does not exist', $source));
-      }
-      $reflection = new ReflectionClass($source);
-      // that class must be a child of Propels BaseObject
-      if (!$reflection->isSubclassOf('BaseObject'))
-      {
-        throw new UnexpectedValueException(sprintf('Class "%s" is no Propel based class', $source));
-      }
-      
-      $tmp = new $source();
-      $peer = $tmp->getPeer();
-      $tableMap = call_user_func_array(array($peer, 'getTableMap'), array());
-      
-      $this->countCriteria = new Criteria();
-      $this->countCriteria->setPrimaryTableName($tableMap->getName());
-      call_user_func_array(array($peer, 'addSelectColumns'), array($this->countCriteria));
-      
-      $this->selectCriteria = clone $this->countCriteria;
-      
-      
-      $peer->addSelectColumns($this->selectCriteria);
+      $source = func_get_args();
     }
-    // ...the source can also be passed as Criteria ...
+    
+    // if the source is provided as object paths, create hydratable criteria 
+    if (is_array($source))
+    {
+      // generate an array of classes to be retrieved from DB
+      $classes = array();
+      foreach ($source as $objectPath)
+      {
+        // test if relations are valid
+        self::checkObjectPath($objectPath);
+
+        $classes =  self::resolveAllClasses($objectPath, $classes);
+      }
+      
+      // process all classes
+      foreach ($classes as $class)
+      {
+         //addSelectColumns
+         
+         //join related
+         
+        
+        
+        
+        
+        // construct full hydration-profile
+//        $this->countCriteria = new Criteria();
+//        $this->selectCriteria = clone $this->countCriteria;
+        
+        //TODO: below
+//        $tmp = new $source();
+//        $peer = $tmp->getPeer();
+//        $tableMap = call_user_func_array(array($peer, 'getTableMap'), array());
+//        
+//        $this->countCriteria = new Criteria();
+//        $this->countCriteria->setPrimaryTableName($tableMap->getName());
+//        call_user_func_array(array($peer, 'addSelectColumns'), array($this->countCriteria));
+//        
+//        $this->selectCriteria = clone $this->countCriteria;
+//        
+//        
+//        $peer->addSelectColumns($this->selectCriteria);        
+      }
+      
+      print_r($classes);
+      die ('<br>tot nu toe goed... genoeg.');
+        
+    }
+    // ...the source can also be passed as custom criteria, these will not be hydrated!
     elseif ($source instanceof Criteria)
     {
       if (!$countCriteria instanceof Criteria)
